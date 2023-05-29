0f65c7b8ef7a92aea73de78d0d49a595fcb11b38
dcy11011 <dcy11011@foxmail.com>
2023-05-29 10:08:50 +0800

    fix bug that plant loos health when preview removing plant

--- Assets/Scripts/Tilemap/HexTilemap.cs
+++ Assets/Scripts/Tilemap/HexTilemap.cs
@@ -525,13 +525,8 @@ public class HexTilemap : MonoBehaviour
                 }
             }
         }
-        
-    }
-
-
-    private void FixedUpdate()
-    {
-        foreach(var Tile in tiles.Values)
+        ClearAllTempValue();
+        foreach (var Tile in tiles.Values)
         {
             var tile = Tile.GetComponent<HexTile>();
             if (tile != null && tile.Plant != null)
@@ -539,9 +534,16 @@ public class HexTilemap : MonoBehaviour
                 var plant = tile.Plant.GetComponent<Plant>();
                 if (plant != null && !tile.CheckPlantCanGrow(plant, plant.rotateCnt))
                 {
-                    plant.LooseHelth(Time.fixedDeltaTime); // 减少枯萎植物的生命值
+                    plant.LooseHelth(Time.deltaTime); // 减少枯萎植物的生命值
                 }
             }
         }
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        
     }
 }

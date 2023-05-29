0c0a7b135dcb083b6d58c9eebc729133f92cc010
Y.S. Chern <alphagem618@gmail.com>
2023-05-29 11:07:20 +0800

    images fix

--- Assets/Scripts/Sidebar/SubSidebarImage.cs
+++ Assets/Scripts/Sidebar/SubSidebarImage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class SubSidebarImage : MonoBehaviour
 {
@@ -32,6 +33,35 @@ public class SubSidebarImage : MonoBehaviour
     public void OnHover()
     {
         sidebar.hintbar.GetComponent<Image>().sprite = sidebar.hintbarSprites[id - 1];
+        var intros = new string[]{
+            "使一个相邻地块的肥力等级+1",
+                "使三个相邻地块的肥力等级+1",
+                "根据相邻地块的肥力等级来增幅另外一个地块",
+                "根据相邻地块的肥力等级来增幅另外三个地块",
+                "根据相邻地块的肥力等级来增幅多个地块，至衰减为0",
+                "同时产出种子与果实\r\n效率尚可",
+                "同时产出种子与果实\r\n效率较高",
+                "中心占用地块的肥力等级越高，产种效率越高",
+                "散发着芬芳气味的娇小花朵，能够吸引昆虫",
+                "舒展着美丽花瓣的奇异花朵，能够吸引鸟儿",
+                "传说中的巨大花朵，其名讳无法拼读，巨龙凭此与同族相互呼唤。但也可能引来别的什么……"
+        };
+        sidebar.hintbar.GetComponent<Hintbar>().intro.GetComponent<TMP_Text>().text = intros[id - 1];
+        var mottos = new string[]
+        {
+            "每10s产出1颗种子",
+            "每20s产出1颗种子",
+            "每30s产出1颗种子",
+            "每40s产出1颗种子",
+            "每40s产出1颗种子",
+            "每10s产出1颗种子，1颗果实",
+            "每30s产出3颗种子，30颗果实",
+            "每30s产出3颗种子，3X颗果实",
+            "",
+            "",
+            ""
+        };
+        sidebar.hintbar.GetComponent<Hintbar>().motto.GetComponent<TMP_Text>().text = mottos[id - 1];
         sidebar.hintbar.SetActive(true);
     }
 
